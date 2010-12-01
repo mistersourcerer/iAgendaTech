14a05cb3b83600a3217b00b9bc37fa4f58934160
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2010-12-01 00:22:15 -0200

    initial commit

---
+++ Classes/AgendaTechClientAppDelegate.h
@@ -0,0 +1,32 @@
+//
+//  AgendaTechClientAppDelegate.h
+//  AgendaTechClient
+//
+//  Created by Ricardo Valeriano dos Santos on 12/1/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AgendaTechClientAppDelegate : NSObject <UIApplicationDelegate> {
+    
+    UIWindow *window;
+    
+@private
+    NSManagedObjectContext *managedObjectContext_;
+    NSManagedObjectModel *managedObjectModel_;
+    NSPersistentStoreCoordinator *persistentStoreCoordinator_;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (NSString *)applicationDocumentsDirectory;
+- (void)saveContext;
+
+@end
+
