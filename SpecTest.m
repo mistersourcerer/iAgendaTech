3dcc339a79862093d23a828d317141809dd1a74b
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2010-12-03 19:21:53 -0200

    added the 'workaround' for tests: http://stackoverflow.com/questions/3516745/sentestcase-in-xcode-3-2-and-xcbuildlogcommandinvocationsection-errors

--- SpecTest.m
+++ SpecTest.m
@@ -1,35 +1,30 @@
-//
-//  SpecTest.m
-//  AgendaTechClient
-//
-//  Created by Ricardo Valeriano dos Santos on 12/1/10.
-//  Copyright 2010 __MyCompanyName__. All rights reserved.
-//
+#import "Kiwi.h"
 
-#import "SpecTest.h"
+SPEC_BEGIN(TestSpec)
 
+describe(@"A simple test", ^{
+    it(@"works", ^{
+        // Try changing should to shouldNot, and vice-versa to see
+        // failures in action.
+		
+        id anArray = [NSArray arrayWithObject:@"Foo"];
+        [[anArray should] contain:@"Foo"];
+        [[anArray shouldNot] contain:@"Bar"];
+		
+        [[theValue(42) should] beGreaterThan:theValue(10.0f)];
+        [[theValue(42) shouldNot] beLessThan:theValue(20)];
+		
+        id scannerMock = [NSScanner mock];
+        [[scannerMock should] receive:@selector(setScanLocation:)];
+        [scannerMock setScanLocation:10];
+		
+        [scannerMock stub:@selector(string) andReturn:@"Unicorns"];
+        [[[scannerMock string] should] equal:@"Unicorns"];
+		
+        [[theBlock(^{
+            [NSException raise:NSInternalInconsistencyException format:@"oh-oh"];
+        }) should] raise];
+    });
+});
 
-@implementation SpecTest
-
-#if USE_APPLICATION_UNIT_TEST     // all code under test is in the iPhone Application
-
-- (void) testAppDelegate {
-    
-    id yourApplicationDelegate = [[UIApplication sharedApplication] delegate];
-    STAssertNotNil(yourApplicationDelegate, @"UIApplication failed to find the AppDelegate");
-    
-}
-
-#else                           // all code under test must be linked into the Unit Test bundle
-
-- (void) testMath {
-    
-    STAssertTrue((1+1)==2, @"Compiler isn't feeling well today :-(" );
-    
-}
-
-
-#endif
-
-
-@end
+SPEC_END
