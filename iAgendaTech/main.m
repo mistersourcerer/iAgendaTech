a6d7a97ed406a35c1c6bc6aff7e481d9468087d2
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2011-05-10 19:05:34 -0300

    adiciona delay para execução da spec, para dar tempo da aplicação subir

--- iAgendaTech/main.m
+++ iAgendaTech/main.m
@@ -10,10 +10,9 @@
 #import "UISpec.h"
 
 int main(int argc, char *argv[]) {
-    
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 
-	[UISpec runSpecs];
+	[UISpec runSpecsAfterDelay:3];
 	
     int retVal = UIApplicationMain(argc, argv, nil, nil);
     [pool release];
